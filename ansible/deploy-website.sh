ff087c5092edbe52b4fea04f3636e60532c0a92d
kthure01 <kent.ho.thureson@gmail.com>
2021-04-12 15:36:37 +0200

    added website deploy

--- ansible/deploy-website.sh
+++ ansible/deploy-website.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/bash
 
-ansible-playbook -i playbooks/inventory.yml playbooks/install-lighttpd.yml
 ansible-playbook -i playbooks/inventory.yml playbooks/deploy-website.yml
--- ansible/install-jenkins-ansible.sh
+++ ansible/install-jenkins-ansible.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
-ansible-playbook  -i inventory.yml playbooks/install-jenkins.yml
-ansible-playbook  -i inventory.yml playbooks/install-ansible.yml
+ansible-playbook -i playbooks/inventory.yml playbooks/install-jenkins.yml
+ansible-playbook -i playbooks/inventory.yml playbooks/install-ansible.yml
