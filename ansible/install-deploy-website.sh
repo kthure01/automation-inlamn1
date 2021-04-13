814d021b3e3f58b478f10e479954d002b297a313
kthure01 <kent.ho.thureson@gmail.com>
2021-04-13 08:51:02 +0200

    updated

--- ansible/install-deploy-website.sh
+++ ansible/install-deploy-website.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 
 dir=$(dirname $0)
+echo "dir: $dir"
 ansible-playbook -i $dir/playbooks/inventory.yml $dir/playbooks/install-lighttpd.yml
 ansible-playbook -i $dir/playbooks/inventory.yml $dir/playbooks/deploy-website.yml
--- ansible/install-jenkins-ansible.sh
+++ ansible/install-jenkins-ansible.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 
 dir=$(dirname $0)
+echo "dir: $dir"
 ansible-playbook -i $dir/playbooks/inventory.yml $dir/playbooks/install-jenkins.yml
 ansible-playbook -i $dir/playbooks/inventory.yml $dir/playbooks/install-ansible.yml
